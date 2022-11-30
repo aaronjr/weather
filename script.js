978ea53db31177cf81531d25d5fffcb02c4e22a8
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-11-30 10:26:23 +0000

    create files

---
+++ script.js
@@ -0,0 +1,7 @@
+// 8b05adff7a43d479faf0fb11bb35a2d8
+// https://api.openweathermap.org/data/2.5/weather?q=London&APPID=8b05adff7a43d479faf0fb11bb35a2d8
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+    
+})
